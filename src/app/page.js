03266233cfc8dc21aa9e828991bbf8f9bbbe1d27
Gurkirat Singh <gurkiratsingh12a@gmail.com>
2024-11-11 14:16:55 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import FeaturesSection from "@/components/ui/FeaturesSection";
+// import FeaturesSection from "@/components/ui/FeaturesSection";
 import HeroSection from "@/components/ui/HeroSection";
-import WhyChooseUs from "@/components/ui/WhyChooseUs";
-import CTASection from "@/components/ui/Cta";
-import StatsDivider from "@/components/UI/StatsDivider";
+// import WhyChooseUs from "@/components/ui/WhyChooseUs";
+// import CTASection from "@/components/ui/Cta";
+// import StatsDivider from "@/components/UI/StatsDivider";
 import TestimonialsSection from "@/components/UI/Testimonials";
 import Services from "@/components/UI/Services";
-import LogoScroller from "@/components/UI/LogoScroller";
+// import LogoScroller from "@/components/UI/LogoScroller";
 import HowItWorks from "@/components/UI/HowItWorks(new)/HowItWorks";
 import BlogSection from "@/components/UI/Blog";
 import FAQ from "@/components/UI/Faq";
