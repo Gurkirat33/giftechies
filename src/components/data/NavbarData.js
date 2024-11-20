2b0fb0ddfe48e2b1bc46adc3d9d21b7e168ddfab
Gurkirat Singh <gurkiratsingh13a@gmail.com>
2024-11-20 12:00:42 +0530

    navbar fix

--- src/components/data/NavbarData.js
+++ src/components/data/NavbarData.js
@@ -15,27 +15,27 @@ export const navData = [
         {
           title: "Case Studies",
           description: "Explore our successful projects",
-          href: "/work/case-studies",
+          href: "/",
         },
         {
           title: "Portfolio",
           description: "Browse our latest work",
-          href: "/work/portfolio",
+          href: "/",
         },
         {
           title: "Blogs",
           description: "Read our latest blog posts",
-          href: "/work/blog",
+          href: "/",
         },
         {
           title: "Client Testimonials",
           description: "See what our clients have to say",
-          href: "/work/testimonials",
+          href: "/",
         },
       ],
       preview: {
         image:
-          "https://imgs.search.brave.com/AynPTPoUajFXwF1GgWBrEP6Fyb0D9y-je4iTrHdQ4Gs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE2/ODQzMjM3MzA2ODct/MjI4MzBlMzgyMTA2/P2ZtPWpwZyZxPTYw/Jnc9MzAwMCZpeGxp/Yj1yYi00LjAuMyZp/eGlkPU0zd3hNakEz/ZkRCOE1IeHpaV0Z5/WTJoOE1UaDhmSGR2/Y210cGJtY2xNakJ0/WVc1OFpXNThNSHg4/TUh4OGZEQT0",
+          "/about3.jpeg",
         title: "Our Work",
         description: "Discover how we've helped businesses grow",
       },
